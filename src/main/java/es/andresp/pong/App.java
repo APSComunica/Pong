881e1528b8e807c4b390a607351f1c33af430295
Usuario <Usuario@Usuario-VAIO>
2020-01-14 14:31:21 +0100

    pala moviendose

--- src/main/java/es/andresp/pong/App.java
+++ src/main/java/es/andresp/pong/App.java
@@ -6,10 +6,12 @@ import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
-import static javafx.scene.paint.Color.BLACK;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Shape;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
@@ -26,10 +28,20 @@ public class App extends Application {
     int ballCurrentSpeedY = 5;
     int ballDirectionY = 1;
     
+    
+    final int SCENE_TAM_X = 640;
+    final int SCENE_TAM_Y = 480;
+    final int STICK_WIDTH = 7;
+    final int STICK_HEIGHT = 50;
+    int stickPosY = (SCENE_TAM_Y - STICK_HEIGHT) / 2;
+
+    int stickCurrentSpeed = 0;
+    
+    
     @Override
     public void start(Stage stage) {
         Pane root = new Pane();
-        var scene = new Scene(root, 640, 480);
+        var scene = new Scene(root, SCENE_TAM_X, SCENE_TAM_Y);
         scene.setFill(Color.BLACK);
         stage.setScene(scene);
         stage.show();
@@ -42,6 +54,13 @@ public class App extends Application {
         circleBall.setRadius(7);
         circleBall.setFill(Color.WHITE);
         root.getChildren().add(circleBall);
+        
+        //rectangulo 1
+        Rectangle rectStick = new Rectangle(SCENE_TAM_X*0.9, stickPosY, STICK_WIDTH, STICK_HEIGHT);
+        rectStick.setFill(Color.WHITE);
+        root.getChildren().add(rectStick);
+        
+        
         
       Timeline timeline = new Timeline(
             // 0.017 ~= 60 FPS
@@ -64,11 +83,64 @@ public class App extends Application {
                         ballDirectionY = 1;
                         
                     }
+                    
+                    if(ballCenterX >= SCENE_TAM_X) {
+                        
+                    
+                    }
+                    
+                    //JOISTICK
+        scene.setOnKeyPressed((KeyEvent event) -> {
+            switch(event.getCode()){
+                case UP:
+                //PULSADA TECLA ARRIBA
+                    stickCurrentSpeed = -6;
+                    break;
+                case DOWN:
+                //PULSADA TECLA ABAJO
+                    stickCurrentSpeed = 6;
+                    break;
+            }
+        }); 
+    
+        scene.setOnKeyReleased((KeyEvent event) -> {
+            stickCurrentSpeed = 0;
+        });
+     
+        //actualizar posicion de la pala
+        stickPosY += stickCurrentSpeed;
+        if(stickPosY < 0) {
+            //No sobrepasar borde superior de la ventana
+            stickPosY = 0;
+        } else {
+            // No sobrepasar borde inferior de la ventana
+           if(stickPosY > SCENE_TAM_Y - STICK_HEIGHT) {
+                stickPosY = SCENE_TAM_Y - STICK_HEIGHT;
+            }
+        }
+        rectStick.setY(stickPosY);
+        
                 }
             })                
         );
         timeline.setCycleCount(Timeline.INDEFINITE);
-        timeline.play();   
+        timeline.play();  
+        
+        
+        
+   Shape.intersect(circleBall, rectStick);     
+   Shape shapeColision = Shape.intersect(circleBall, rectStick);
+   boolean colisionVacia = shapeColision.getBoundsInLocal().isEmpty();
+   if(colisionVacia == false) {
+       //colision detectada. Mover bola hacia izquierda
+        ballCurrentSpeedX = -3;
+        
+    }     
+        
+        
+        
+        
+        
     }
 
     public static void main(String[] args) {
